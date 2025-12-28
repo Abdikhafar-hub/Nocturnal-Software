932867e57293c940773fb480905584933445af99
Abdikhafar Issack <abdikhafarissack@gmail.com>
2025-12-28 10:59:15 +0300

    k

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Space_Grotesk } from "next/font/google"
-import Script from "next/script"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 import StructuredData from "@/components/structured-data"
@@ -99,11 +98,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-sans antialiased bg-background text-foreground`}>
+      <head>
         {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -112,6 +109,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-TWH2XV6R');`,
           }}
         />
+      </head>
+      <body className={`${inter.variable} font-sans antialiased bg-background text-foreground`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
